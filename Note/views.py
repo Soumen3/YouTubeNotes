2e7b44b25c68716efdf664e9d48e4cd218f47971
Soumen3 <soumensamanta112233@gmail.com>
2024-08-06 23:15:14 +0530

    pages add and url defined

--- Note/views.py
+++ Note/views.py
@@ -3,3 +3,10 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
 	return render(request, 'Note/home.html')
+
+
+def videos(request):
+	return render(request, 'Note/videos.html')
+
+def notes(request):
+	return render(request, 'Note/notes.html')
